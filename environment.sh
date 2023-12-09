d6df03271324d6d40714ae28a66bf40f46553188
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-09 21:04:28 +0000

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
+: << 'environment'
+The environment in focus
+environment
+prefix=/opt/miniconda3/envs/climate
 
-conda remove --prefix /opt/miniconda3/envs/climate --all
+: << 'delete'
+Delete the existing <climate> environment
+delete
+conda remove -y --prefix $prefix --all
 
-: << 'yml'
-Via an environment.yml file
-yml
-# conda env create --file environment.yml -p /opt/miniconda3/envs/climate
-
-
-: << 'requirements'
-Via a requirements.txt file
-requirements
-
-# Initiate an environment
-conda create -y --prefix /opt/miniconda3/envs/climate python==3.11.7
-
-# Install the
-conda install -y --prefix /opt/miniconda3/envs/climate -c anaconda --file requirements.txt
-
-# If a package is only available via pip
-# conda activate climate
-# pip install ... --no-cache-dir
-# pip install ... --no-cache-dir
-# conda deactivate
+: << 'rebuild'
+Rebuild environment <climate> via a requirements.txt file
+rebuild
+conda create -y --prefix $prefix  python==3.11.7
+conda install -y --prefix $prefix -c anaconda --file requirements.txt
