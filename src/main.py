99c44abea03cd8f211c73e52e37f213305dab87f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-17 22:53:01 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -2,15 +2,28 @@
 import logging
 import os
 import sys
+import collections
 
 def main():
     """
     Entry point
     """
 
+    # Log
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('pollutants')
 
+    # Try
+    datestr = '2023-11-18'
+    url: str = src.data.url.URL().exc(datestr=datestr)
+    logger.info(url)
+    sample = src.functions.objects.Objects().api(url=url)
+    logger.info(sample.__getitem__('name'))
+    logger.info(sample.__getitem__('data'))
+
+    # Deleting __pycache__
+    src.functions.cache.Cache().delete()
+    
 
 if __name__ == '__main__':
 
@@ -23,5 +36,10 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # Modules
+    import src.functions.objects
+    import src.functions.cache
+    import src.data.url
 
     main()
