13ec0a94035f43cadcf2b4cf647ae2b351fdc3b2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-17 20:37:35 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+"""Module main.py"""
 import logging
 import os
 import sys
 
 def main():
+    """
+    Entry point
+    """
 
+    logger: logging.Logger = logging.getLogger(__name__)
     logger.info('pollutants')
 
 
@@ -18,6 +23,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger(__name__)
 
     main()
