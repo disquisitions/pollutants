f7f980b95ea0442f0bedb8a34418010749de962d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-22 16:37:39 +0000

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -6,15 +6,15 @@ requirements.txt file to create an environment.
 comment
 
 # The environment in focus
-prefix=/opt/miniconda3/envs/climate
+prefix=/opt/miniconda3/envs/pollutants
 
 : << 'delete'
-  Delete the existing <climate> environment
+  Delete the existing <pollutants> environment
 delete
 conda remove -y --prefix $prefix --all
 
 : << 'rebuild'
-  Rebuild environment <climate> via a requirements.txt file
+  Rebuild environment <pollutants> via a requirements.txt file
 rebuild
-conda create -y --prefix $prefix -c conda-forge  python==3.11.6
+conda create -y --prefix $prefix -c conda-forge  python==3.11.7
 conda install -y --prefix $prefix -c conda-forge --file requirements.txt
