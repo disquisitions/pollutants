3f76d92b8d20c1ff6b53ccc11643c297de17761e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-22 21:29:50 +0000

    Update src/data/pollutants.py

--- src/data/pollutants.py
+++ src/data/pollutants.py
@@ -1,5 +1,26 @@
+import src.functions.objects
+import logging
 
 class Pollutants:
 
     def __init__(self) -> None:
-        pass
+        """
+
+        """
+        self.__url: str = 'https://www.scottishairquality.scot/sos-scotland/api/v1/phenomena'
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        objects = src.functions.objects.Objects()
+        data: dict = objects.api(url=self.__url)
+        self.__logger.info(data)
