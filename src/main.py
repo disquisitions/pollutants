9395673f0b6e977e9783dbbb51de58cdaea46971
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-23 23:22:15 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -14,9 +14,10 @@ def main():
     logger.info('Pollutants')
 
     # Try
+    # pollutant_id = 1
     sequence_id = 214
     datestr = '2023-11-18'
-    url: str = src.data.url.URL().exc(sequence_id=sequence_id, datestr=datestr)
+    url: str = src.data.api.API().exc(sequence_id=sequence_id, datestr=datestr)
     logger.info(url)
     sample = src.functions.objects.Objects().api(url=url)
     logger.info(sample[0].__getitem__('name'))
@@ -51,6 +52,6 @@ if __name__ == '__main__':
     import src.data.stations
     import src.data.vocabulary
     import src.data.sequences
-    import src.data.url
+    import src.data.api
 
     main()
