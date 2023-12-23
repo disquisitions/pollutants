75cb376294c46f10510a1e12a99a2c7c2625ef04
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-23 18:21:04 +0000

    Update src/data/sequences.py

--- src/data/sequences.py
+++ src/data/sequences.py
@@ -12,6 +12,10 @@ class Sequences:
 
         self.__url = 'https://www.scottishairquality.scot/sos-scotland/api/v1/timeseries'
 
+        labels = ['id', 'label', 'uom', 'station.properties.id', 'station.properties.label']
+        names = ['sequence_id', 'description', 'unit_of_measure', 'station_id', 'station_label']
+        self.__rename = dict(zip(labels, names))
+
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -22,9 +26,21 @@ class Sequences:
     def __structure(blob: dict) -> pd.DataFrame:
 
         try:
-            return pd.json_normalize(data=blob, max_level=2)
+            normalised = pd.json_normalize(data=blob, max_level=2)
         except ImportError as err:
             raise Exception(err) from err
+
+        coordinates = pd.DataFrame(data=normalised['station.geometry.coordinates'].to_list(),
+                                   columns=['longitude', 'latitude', 'height'])
+        data = normalised.copy().drop(columns='station.geometry.coordinates').join(coordinates, how='left')
+        data.drop(columns=['station.type', 'station.geometry.type', 'height'], inplace=True)
+
+        return data
+
+    def __feature_engineering(self, blob: pd.DataFrame):
+
+        identifiers = blob.copy()['description'].str.split(n=1, expand=True)
+        self.__logger.info(identifiers.loc[0, 0])
 
     def exc(self):
 
@@ -32,5 +48,7 @@ class Sequences:
         dictionary = objects.api(url=self.__url)
 
         data = self.__structure(blob=dictionary)
+        data.rename(columns=self.__rename, inplace=True)
+        self.__feature_engineering(blob=data)
         self.__logger.info(data.info())
         self.__logger.info(data.head())
