ad59d05f68a622ca97b96c9bb62f36b618f6f5f1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-24 19:50:07 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,18 +1,22 @@
 
 import logging
+import os
 
 import pandas as pd
 
 import src.data.references
 import src.elements.sequence
+import src.functions.directories
 
 
 class Interface:
 
     def __init__(self):
         """
-
+        Constructor
         """
+
+        self.__storage = os.path.join(os.getcwd(), 'warehouse', 'pollutants')
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -20,11 +24,18 @@ class Interface:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, pollutant_id: int):
+    def exc(self, pollutant_id: int, restart: bool = False):
         """
 
         :return:
         """
+
+        path = os.path.join(self.__storage, str(pollutant_id))
+
+        if restart:
+            directories = src.functions.directories.Directories()
+            directories.cleanup(path=path)
+            directories.create(path=path)
 
         collection = src.data.references.References().exc()
 
