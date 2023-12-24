3f720048e92e9641842c01a902e8bf4c383f653f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-24 19:50:18 +0000

    Update src/data/points.py

--- src/data/points.py
+++ src/data/points.py
@@ -1,30 +1,55 @@
+"""Module points"""
+import os
+
 import dask
 import pandas as pd
 
 import src.data.api
 import src.elements.sequence
 import src.functions.objects
+import src.functions.streams
+import src.functions.directories
 
 
 class Points:
+    """
+    Class Points
+    Retrieves telemetric device's data points by date
+    """
 
-    def __init__(self, sequences: list[src.elements.sequence.Sequence]):
+    def __init__(self, sequences: list[src.elements.sequence.Sequence], path: str):
         """
 
+        :param sequences:
+        :param path:
         """
 
         self.__sequences = sequences
+        self.__path = path
 
         self.__api = src.data.api.API()
         self.__objects = src.functions.objects.Objects()
+        self.__streams = src.functions.streams.Streams()
+        self.__directories = src.functions.directories.Directories()
 
     @dask.delayed
     def __url(self, sequence_id: int, datestr: str) -> str:
+        """
+
+        :param sequence_id:
+        :param datestr:
+        :return:
+        """
 
         return self.__api.exc(sequence_id=sequence_id, datestr=datestr)
 
     @dask.delayed
     def __read(self, url: str) -> dict:
+        """
+
+        :param url:
+        :return:
+        """
 
         content: dict = self.__objects.api(url=url)
         dictionary = content[0].__getitem__('data')
@@ -33,6 +58,11 @@ class Points:
 
     @dask.delayed
     def __build(self, dictionary: dict) -> pd.DataFrame:
+        """
+
+        :param dictionary:
+        :return:
+        """
 
         data = pd.DataFrame(data=dictionary, columns=['epoch_ms', 'measure'])
         data.loc[:, 'timestamp'] = pd.to_datetime(data.loc[:, 'epoch_ms'].array, unit='ms', origin='unix')
@@ -40,11 +70,32 @@ class Points:
 
         return data
 
+    @dask.delayed
+    def __write(self, blob: pd.DataFrame, datestr: str, station_id: int) -> str:
+        """
+
+        :param blob:
+        :param datestr:
+        :param station_id:
+        :return:
+        """
+
+        basename = os.path.join(self.__path, str(station_id))
+        self.__directories.create(path=basename)
+
+        return self.__streams.write(blob=blob, path=os.path.join(basename, f'{datestr}.csv'))
+
     def exc(self, datestr: str):
+        """
+
+        :param datestr:
+        :return:
+        """
 
         computations = []
         for sequence in self.__sequences:
             url = self.__url(sequence_id=sequence.sequence_id, datestr=datestr)
             dictionary = self.__read(url=url)
             data = self.__build(dictionary=dictionary)
-            computations.append(data)
+            message = self.__write(blob=data, datestr=datestr, station_id=sequence.station_id)
+            computations.append(message)
