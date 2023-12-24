64b259e77b4bb61f061881ffad03881a3f8400ac
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-24 16:14:35 +0000

    Update src/data/interface.py: In progress

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,4 +1,9 @@
 
+import logging
+
+import src.data.references
+
+
 class Interface:
 
     def __init__(self):
@@ -6,8 +11,22 @@ class Interface:
 
         """
 
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
     def exc(self, pollutant_id: int):
         """
 
         :return:
         """
+
+        collection = src.data.references.References().exc()
+
+        sequences = collection.sequences
+        self.__logger.info(sequences.info())
+
+        excerpt = sequences.loc[sequences['pollutant_id'] == pollutant_id, :]
+        self.__logger.info(excerpt.info())
