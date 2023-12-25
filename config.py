82048ea938cd76df6accf7c04931ed7c69cd52bf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-25 15:16:45 +0000

    Add config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+class Config:
+
+    def __init__(self):
+        """
+
+        """
+
+        # S3 Express One Zone, which has 4 overarching regions
+        # https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-express-Regions-and-Zones.html
+        region = 'eu-north-1'
+        availability_zone = 'eun1-az3'
+        regional_endpoint = 's3express-control.eu-north-1.amazonaws.com'
+        zonal_endpoint = f's3express-eun1-az1.{region}.amazonaws.com'
