289d530f739fdff163439b3967959012ad865b1a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-26 12:44:25 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import collections
+
 
 class Config:
 
@@ -8,11 +10,26 @@ class Config:
 
         # S3 Express One Zone, which has 4 overarching regions
         # https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-express-Regions-and-Zones.html
-        region = 'eu-north-1'
+        region_code = 'eu-north-1'
         availability_zone = 'eun1-az3'
-        regional_endpoint = 's3express-control.eu-north-1.amazonaws.com'
-        zonal_endpoint = f's3express-eun1-az1.{region}.amazonaws.com'
-
-        # bucket base
-        bucket_base_name = 'pollutants/{pollutant_id}/{station_id}/{datestr}.csv'
+        regional_endpoint = f'https://s3express-control.{region_code}.amazonaws.com'
+        zonal_endpoint = f'https://s3express-{availability_zone}.{region_code}.amazonaws.com'
         bucket_base_name_affix = f'--{availability_zone}--x-s3'
+
+        # Data
+        DataPartition = collections.namedtuple(
+            typename='DataPartition', field_names=['bucket_base_name', 'bucket_base_name_affix',
+                                                   'regional_endpoint', 'zonal_endpoint'])
+        DataPartition(bucket_base_name='pollutants/points/{pollutant_id}/{station_id}/{datestr}.csv',
+                      bucket_base_name_affix=bucket_base_name_affix,
+                      regional_endpoint=regional_endpoint,
+                      zonal_endpoint=zonal_endpoint)
+
+        # References
+        ReferencesPartition = collections.namedtuple(
+            typename='ReferencesPartition', field_names=['bucket_base_name', 'bucket_base_name_affix',
+                                                         'regional_endpoint', 'zonal_endpoint'])
+        ReferencesPartition(bucket_base_name='pollutants/references/{filename}.csv',
+                            bucket_base_name_affix=bucket_base_name_affix,
+                            regional_endpoint=regional_endpoint,
+                            zonal_endpoint=zonal_endpoint)
