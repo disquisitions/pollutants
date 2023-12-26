e02434ee6732efe146997240cf95fc74328207e6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-26 20:06:25 +0000

    Update src/references/interface.py

--- src/references/interface.py
+++ src/references/interface.py
@@ -5,6 +5,8 @@ import pandas as pd
 import src.references.sequences
 import src.references.stations
 import src.references.substances
+import src.elements.connector
+import src.s3.bucket
 
 
 class Interface:
@@ -14,19 +16,26 @@ class Interface:
     Rebuild, or retrieve the Amazon S3 data?  The Amazon S3 aspect is upcoming.
     """
 
-    def __init__(self):
+    def __init__(self, parameters: src.elements.connector.Connector):
         """
         Constructor
         """
 
-        self.__bucket_base_name = 'pollutants/references/'
+        self.__bucket_base_name = 'pollutants'
+        self.__parameters = parameters
 
-    @staticmethod
-    def exc() -> None:
+    def __bucket(self):
+
+        bucket_name = self.__bucket_base_name + self.__parameters.bucket_base_name_affix
+        src.s3.bucket.Bucket(parameters=self.__parameters).create(bucket_name=bucket_name)
+
+    def exc(self) -> None:
         """
 
         :return:
         """
+
+        self.__bucket()
 
         src.references.substances.Substances().exc()
         src.references.stations.Stations().exc()
