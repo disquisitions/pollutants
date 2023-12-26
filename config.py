2094c5f2d6952f536162f578fdefaf613615b6ca
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-26 17:11:49 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import collections
-
 
 class Config:
 
@@ -7,31 +5,3 @@ class Config:
         """
 
         """
-
-        # S3 Express One Zone, which has 4 overarching regions
-        # https://docs.aws.amazon.com/AmazonS3/latest/userguide/s3-express-Regions-and-Zones.html
-        region_name = 'eu-north-1'
-        availability_zone = 'eun1-az3'
-        regional_endpoint = f'https://s3express-control.{region_name}.amazonaws.com'
-        zonal_endpoint = f'https://s3express-{availability_zone}.{region_name}.amazonaws.com'
-        bucket_base_name_affix = f'--{availability_zone}--x-s3'
-
-        # Data
-        PartitionOfPoints = collections.namedtuple(
-            typename='PartitionOfPoints', field_names=['bucket_base_name', 'bucket_base_name_affix',
-                                                       'regional_endpoint', 'zonal_endpoint'])
-        partition_of_points = PartitionOfPoints(
-            bucket_base_name='pollutants/points/{pollutant_id}/{station_id}/{datestr}.csv',
-            bucket_base_name_affix=bucket_base_name_affix,
-            regional_endpoint=regional_endpoint,
-            zonal_endpoint=zonal_endpoint)
-
-        # References
-        PartitionOfReferences = collections.namedtuple(
-            typename='PartitionOfReferences', field_names=['bucket_base_name', 'bucket_base_name_affix',
-                                                           'regional_endpoint', 'zonal_endpoint'])
-        partition_of_references = PartitionOfReferences(
-            bucket_base_name='pollutants/references/{filename}.csv',
-            bucket_base_name_affix=bucket_base_name_affix,
-            regional_endpoint=regional_endpoint,
-            zonal_endpoint=zonal_endpoint)
