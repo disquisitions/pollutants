2f78ffcbddc9ebd737c43b0c3377f3e338333cee
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-27 16:03:35 +0000

    Update src/s3/bucket.py

--- src/s3/bucket.py
+++ src/s3/bucket.py
@@ -1,49 +1,59 @@
+
 import boto3
 import botocore.exceptions
 
 import src.elements.connector
-import src.s3.profile
 
 
 class Bucket:
 
-    def __init__(self, parameters: src.elements.connector.Connector):
+    def __init__(self, parameters: src.elements.connector.Connector, bucket_name: str):
         """
+        Via resource
+           * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.html#boto3.session.Session.resource
+           * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/index.html
 
         :param parameters:
         """
 
-        profile = src.s3.profile.Profile().exc()
-        boto3.setup_default_session(profile_name=profile)
         self.__parameters = parameters
-        self.__s3_client = boto3.client('s3', region_name=self.__parameters.region_name)
 
-    def create(self, bucket_name: str):
+        # Note, this is optional because the resource instance can build it automatically, via provided parameters
+        endpoint_url = self.__parameters.root.format(bucket_name=bucket_name, region_name=self.__parameters.region_name)
+
+        # The resource instance
+        self.__s3_resource = boto3.resource(service_name='s3', region_name=self.__parameters.region_name,
+                                            endpoint_url=endpoint_url)
+        self.__bucket = self.__s3_resource.Bucket(name=bucket_name)
+
+    def create(self, ):
         """
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/create.html
 
-        :param bucket_name:
+
         :return:
         """
 
+        create_bucket_configuration = {
+            'LocationConstraint': self.__parameters.location_constraint
+        }
+
         try:
-            bucket_configuration = {
-                'Location': {'Type': 'AvailabilityZone', 'Name': self.__parameters.availability_zone},
-                'Bucket': {'Type': 'Directory', 'DataRedundancy': 'SingleAvailabilityZone'}
-            }
-            self.__s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=bucket_configuration)
+            self.__bucket.create(ACL='private',
+                                 CreateBucketConfiguration=create_bucket_configuration)
+            self.__bucket.wait_until_exists()
         except botocore.exceptions.ClientError as err:
             raise Exception(err) from err
 
-    def delete(self, bucket_name: str):
+    def delete(self):
         """
-        https://docs.aws.amazon.com/AmazonS3/latest/userguide/directory-bucket-delete.html
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/objects.html
 
-        :param bucket_name:
         :return:
         """
 
-        # Apriori: aws s3 rm s3://... --recursive
         try:
-            self.__s3_client.delete_bucket(Bucket=bucket_name)
+            self.__bucket.objects.delete()
+            self.__bucket.delete()
         except botocore.exceptions.ClientError as err:
             raise Exception(err) from err
