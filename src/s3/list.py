84034df66dd5c5dc884ceaf278dde5285351cfd0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-27 19:37:35 +0000

    Add src/s3/list.py: A successful inheritance experiment.

---
+++ src/s3/list.py
@@ -0,0 +1,18 @@
+import logging
+
+import src.s3.entities
+
+
+class List(src.s3.entities.Entities):
+
+    def __init__(self):
+        super(List, self).__init__()
+
+        # Logging
+        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger: logging.Logger = logging.getLogger(__name__)
+
+    def exc(self):
+
+        return list(super().resource().buckets.all())
