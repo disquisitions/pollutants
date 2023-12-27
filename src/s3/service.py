c13f4299f9b32f17400e569de355540e0efe1112
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-27 22:45:51 +0000

    Update src/s3/service.py

--- src/s3/service.py
+++ src/s3/service.py
@@ -6,11 +6,18 @@ import logging
 import boto3
 
 import src.elements.parameters
+import src.elements.service
 import src.s3.parameters
 import src.functions.profile
 
 
 class Service:
+    """
+    Via resource
+           * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.\
+                html#boto3.session.Session.resource
+           *
+    """
 
     def __init__(self):
         """
@@ -23,24 +30,10 @@ class Service:
         profile = src.functions.profile.Profile().exc()
         boto3.setup_default_session(profile_name=profile)
 
-        # The parameters
+        # The parameters and the S3 resource
         self.__parameters: src.elements.parameters.Parameters = src.s3.parameters.Parameters().exc()
-
-        # The S3 resource
         self.__s3_resource = boto3.resource(service_name='s3', region_name=self.__parameters.region_name)
 
-    def parameters(self) -> src.elements.parameters.Parameters:
-        """
-
-        :return:
-        """
-
-        return self.__parameters
-
-    def resource(self):
-        """
-
-        :return:
-        """
-
-        return self.__s3_resource
+        # Hence, the collection
+        self.service = src.elements.service.Service(
+            parameters=self.__parameters, s3_resource=self.__s3_resource)
