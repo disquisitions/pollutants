23283923862270306170b4201e0601d7bc0d5781
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-27 12:57:59 +0000

    Update src/functions/directories.py

--- src/functions/directories.py
+++ src/functions/directories.py
@@ -14,7 +14,7 @@ class Directories:
         """
 
     @staticmethod
-    def cleanup(path: str):
+    def cleanup(path: str) -> bool:
         """
         Clears a directory
         :param path:
@@ -33,15 +33,23 @@ class Directories:
                          for directory in directories
                          if os.path.exists(os.path.join(base, directory))]
 
-        if os.path.exists(path=path):
-            os.removedirs(path)
+        try:
+            if os.path.exists(path=path):
+                os.removedirs(path)
+            return True
+        except OSError as err:
+            raise Exception(err) from err
 
     @staticmethod
-    def create(path: str):
+    def create(path: str) -> bool:
         """
         :param path:
         :return:
         """
 
-        if not os.path.exists(path):
-            os.makedirs(path)
+        try:
+            if not os.path.exists(path):
+                os.makedirs(path)
+            return True
+        except OSError as err:
+            raise Exception(err) from err
