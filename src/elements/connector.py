ceecd3cc331445e0ca1ecb1d84a13f867e66416d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-27 11:02:54 +0000

    Update src/elements/connector.py

--- src/elements/connector.py
+++ src/elements/connector.py
@@ -10,8 +10,5 @@ class Connector(typing.NamedTuple):
     """
 
     region_name: str
-    availability_zone: str
-    regional_root: str
-    zonal_root: str
-    root_affix: str
-    bucket_base_name_affix: str
+    location_constraint: str
+    root: str
