30a9079387733c6fc49a26483128cdcd3e0d0ca7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-28 14:45:46 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def main():
     logger.info(hazards)
 
     # Try
-    src.references.interface.Interface(service=service).exc()
+    src.references.interface.Interface(service=service, restart=restart).exc()
 
     # Deleting __pycache__
     src.functions.cache.Cache().delete()
