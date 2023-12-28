5c2adf9cdaf43f032d9bed2ce2da6d0e2cd0f81d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-28 00:10:06 +0000

    Update src/s3/service.py

--- src/s3/service.py
+++ src/s3/service.py
@@ -1,5 +1,5 @@
 """
-Module entities.py
+Module service.py
 """
 import logging
 
@@ -13,26 +13,30 @@ import src.functions.profile
 
 class Service:
     """
-    Via resource
-           * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.\
-                html#boto3.session.Session.resource
-           *
+    Class Service
+
+    Auto-login via IAM Identity Centre Single Sign On; beware of
+    machine prerequisite.  Re-visit, vis-à-vis cloud runs.
+      * https://docs.aws.amazon.com/signin/latest/userguide/command-line-sign-in.html
+
+
+    A S3 resource service
+      * https://boto3.amazonaws.com/v1/documentation/api/latest/reference/core/session.\
+            html#boto3.session.Session.resource
     """
 
     def __init__(self):
         """
-        Re-visit profile in relation to cloud runs.
-
+        Constructor
         """
 
-        # Auto-login via IAM Identity Centre Single Sign On; beware of machine prerequisite.
-        # https://docs.aws.amazon.com/signin/latest/userguide/command-line-sign-in.html
+        # Profile/Auto-login
         profile = src.functions.profile.Profile().exc()
         boto3.setup_default_session(profile_name=profile)
 
         # The parameters and the S3 resource
         self.__parameters: src.elements.parameters.Parameters = src.s3.parameters.Parameters().exc()
-        self.__s3_resource = boto3.resource(service_name='s3', region_name=self.__parameters.region_name)
+        self.__s3_resource: boto3.session.Session.resource = boto3.resource(service_name='s3', region_name=self.__parameters.region_name)
 
         # Hence, the collection
         self.service = src.elements.service.Service(
