ca3f3bd9575eee15459a3fc28427af40d27fba5d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-28 01:52:45 +0000

    Update src/references/interface.py

--- src/references/interface.py
+++ src/references/interface.py
@@ -1,6 +1,8 @@
 """Module interface.py"""
 import os.path
 
+import src.elements.service
+import src.functions.directories
 import src.references.sequences
 import src.references.stations
 import src.references.substances
@@ -13,13 +15,19 @@ class Interface:
     Rebuild, or retrieve the Amazon S3 data?  The Amazon S3 aspect is upcoming.
     """
 
-    def __init__(self):
+    def __init__(self, service: src.elements.service.Service):
         """
 
         """
 
-        # Local
+        # Cloud/Local
+        self.__service = service
+        self.__bucket_name = 'pollutants'
+        self.__key_root = 'references/'
         self.__storage = os.path.join(os.getcwd(), 'warehouse', 'pollutants', 'references')
+
+        directories = src.functions.directories.Directories()
+        directories.create(path=self.__storage)
 
     def exc(self) -> None:
         """
@@ -28,5 +36,7 @@ class Interface:
         """
 
         src.references.substances.Substances().exc()
-        src.references.stations.Stations().exc(storage=self.__storage)
+        src.references.stations.Stations(
+            service=self.__service, storage=self.__storage,
+            bucket_name=self.__bucket_name, key_root=self.__key_root).exc()
         src.references.sequences.Sequences().exc()
