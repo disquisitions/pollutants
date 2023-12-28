cb8d80fce723733fc90e82a67b21de2f4f63e35c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-28 12:21:05 +0000

    Update src/elements/parameters.py

--- src/elements/parameters.py
+++ src/elements/parameters.py
@@ -7,8 +7,19 @@ import typing
 class Parameters(typing.NamedTuple):
     """
     The data type class Parameters
+
+    :parameter:
+      region_name: The Amazon Web Services region code
+      location_constraint: The region code of the region that the data is limited to
+      access_control_list:
+      bucket_name: The Amazon S3 bucket that hosts this project's data
+      points_path: The bucket path of the telemetric data
+      references_path: The bucket path of the telemetric data references
     """
 
     region_name: str
     location_constraint: str
     access_control_list: str
+    bucket_name: str
+    points_path: str
+    references_path: str
