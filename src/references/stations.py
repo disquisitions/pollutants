f47946b07777362966ab833fed96fcdf6ffdbd53
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-28 14:13:31 +0000

    Update src/references/stations.py

--- src/references/stations.py
+++ src/references/stations.py
@@ -1,12 +1,9 @@
 """Module stations.py"""
-import os
-
+import logging
 import pandas as pd
 
-import src.elements.service
+import src.elements.parameters
 import src.functions.objects
-import src.functions.streams
-import src.s3.upload
 
 
 class Stations:
@@ -15,24 +12,22 @@ class Stations:
     Reads-in the Scottish Air Quality Agency's inventory of telemetric devices
     """
 
-    def __init__(self, service: src.elements.service.Service, storage: str, bucket_name: str, key_root: str):
+    def __init__(self):
+        """
+        :var:
+          self.__url: The stations url (uniform resource locator)
+          self.__rename: The original field names of the data, and their corresponding new names.
         """
 
-        :param service: For S3 ...
-        :param storage: Local data depository.
-        :param bucket_name: Amazon S3 bucket name.
-        :param key_root: The key name prefix, i.e., the S3 path of the data file.
-        """
-
-        self.__service = service
-        self.__storage = storage
-        self.__bucket_name = bucket_name
-        self.__key_root = key_root
-
-        # The stations url (uniform resource locator)
-        # The data source field names, and their corresponding new names.
+        # Variables
         self.__url = 'https://www.scottishairquality.scot/sos-scotland/api/v1/stations'
         self.__rename = dict(zip(['properties.id', 'properties.label'], ['station_id', 'station_label']))
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
 
     @staticmethod
     def __structure(blob: dict) -> pd.DataFrame:
@@ -54,35 +49,19 @@ class Stations:
 
         return data
 
-    def __write(self, blob: pd.DataFrame) -> str:
+    @staticmethod
+    def __metadata() -> dict:
         """
-        Locally
-
-        :param blob:
-        :return:
-        """
-
-        streams = src.functions.streams.Streams()
-        pathstr = os.path.join(self. __storage, 'stations.csv')
-
-        return streams.write(blob=blob, path=pathstr)
-
-    def __deliver(self, blob: pd.DataFrame):
-        """
-        Deliver to Amazon S3
 
         :return:
         """
 
-        metadata = {'station_id': 'The identification code of the telemetric device station.',
-                    'station_label': 'Address, etc., details of the station.',
-                    'longitude': 'The x geographic coordinate.', 'latitude': 'The y geographic coordinate.'}
+        return {'station_id': 'The identification code of the telemetric device station.',
+                'station_label': 'Address, etc., details of the station.',
+                'longitude': 'The x geographic coordinate.',
+                'latitude': 'The y geographic coordinate.'}
 
-        upload = src.s3.upload.Upload(service=self.__service)
-        upload.bytes(data=blob, metadata=metadata, bucket_name=self.__bucket_name,
-                     key_name=f'{self.__key_root}stations.csv')
-
-    def exc(self) -> pd.DataFrame:
+    def exc(self) -> (pd.DataFrame, dict):
         """
 
         :return:
@@ -95,9 +74,6 @@ class Stations:
         # Hence, structuring, and renaming the fields in line with field naming conventions and ontology standards.
         data: pd.DataFrame = self.__structure(blob=dictionary)
         data.rename(columns=self.__rename, inplace=True)
+        self.__logger.info('Stations (Above)\n%s\n\n', data.info())
 
-        # Hence
-        self.__write(blob=data)
-        self.__deliver(blob=data)
-
-        return data
+        return data, self.__metadata()
