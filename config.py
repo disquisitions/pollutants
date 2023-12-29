82207adda78c17021d3f219da6866c95444e5ffd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-29 20:20:08 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
+"""
+Module config
+"""
+import os
+
 
 class Config:
+    """
+    Class Config
+
+    For project settings
+    """
 
     def __init__(self):
         """
-
+        Constructor
         """
+
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.hazards = [1, 5]
