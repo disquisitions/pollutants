8d1b557cf0de3dbba02260eb89cc692d90ef7a8f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-29 14:24:15 +0000

    Update src/references/interface.py

--- src/references/interface.py
+++ src/references/interface.py
@@ -3,100 +3,88 @@ import logging
 
 import pandas as pd
 
-import src.elements.parameters
-import src.elements.service
-import src.references.registry
-import src.references.stations
-import src.references.substances
-import src.s3.unload
-import src.s3.upload
+import src.elements.parameters as pr
+import src.elements.sequence as sq
+import src.elements.service as sr
+import src.references.read
+import src.references.regenerate
 
 
 class Interface:
     """
     Class Interface
-
-    Rebuild, or retrieve the Amazon S3 data?  The Amazon S3 aspect is upcoming.
     """
 
-    def __init__(self, service: src.elements.service.Service, restart: bool = False):
+    def __init__(self, service: sr.Service, parameters: pr.Parameters, hazards: list[int]):
         """
 
         :param service:
-        :param restart:
+        :param parameters
+        :param hazards:
         """
 
-        self.__restart = restart
         self.__service = service
+        self.__parameters = parameters
+        self.__hazards = hazards
 
-        # Amazon S3 Settings & Interactions Instances
-        self.__parameters: src.elements.parameters.Parameters = self.__service.parameters
-        self.__unload = src.s3.unload.Unload(service=self.__service)
-        self.__upload = src.s3.upload.Upload(service=self.__service)
-
-    def __stations(self) -> pd.DataFrame:
+    @staticmethod
+    def __integrate(registry: pd.DataFrame, stations: pd.DataFrame, substances: pd.DataFrame) -> pd.DataFrame:
         """
+        Integrates the frames such that each record has the details of each distinct
+        sequence identification code.
 
+        :param registry:
+        :param stations:
+        :param substances:
         :return:
         """
-
-        key_name = f'{self.__parameters.references_}stations.csv'
-
-        if self.__restart:
-            data: pd.DataFrame
-            metadata: dict
-            data, metadata = src.references.stations.Stations().exc()
-            self.__upload.bytes(data=data, metadata=metadata, key_name=key_name)
-        else:
-            chunk = self.__unload.exc(key_name=key_name)
-            data = pd.read_csv(chunk)
-
-        return data
-
-    def __substances(self) -> pd.DataFrame:
-
-        key_name = f'{self.__parameters.references_}substances.csv'
-
-        if self.__restart:
-            data: pd.DataFrame
-            metadata: dict
-            data, metadata = src.references.substances.Substances().exc()
-            self.__upload.bytes(data=data, metadata=metadata, key_name=key_name)
-        else:
-            chunk = self.__unload.exc(key_name=key_name)
-            data = pd.read_csv(chunk)
-
-        return data
-
-    def __registry(self) -> pd.DataFrame:
-
-        key_name = f'{self.__parameters.references_}registry.csv'
-
-        if self.__restart:
-            data: pd.DataFrame
-            metadata: dict
-            data, metadata = src.references.registry.Registry().exc()
-            self.__upload.bytes(data=data, metadata=metadata, key_name=key_name)
-        else:
-            chunk = self.__unload.exc(key_name=key_name)
-            data = pd.read_csv(chunk)
-
-        return data
-
-    def exc(self) -> None:
-        """
-
-        :return:
-        """
-
-        registry = self.__registry()
-        stations = self.__stations()
-        substances = self.__substances().copy()[['pollutant_id', 'substance', 'notation']]
 
         frame = registry.merge(stations, how='left', on='station_id')
-        frame = frame.copy().merge(substances, how='left', on='pollutant_id')
-        conditionals = frame['longitude'].isna() | frame['latitude'].isna()
-        frame = frame.copy().loc[~conditionals, :]
+        frame = frame.copy().merge(
+            substances.copy()[['pollutant_id', 'substance', 'notation']], how='left', on='pollutant_id')
 
-        logging.log(level=logging.INFO, msg=frame.info())
-        logging.log(level=logging.INFO, msg=frame)
+        return frame
+
+    def __sequences(self, blob: pd.DataFrame) -> list[sq.Sequence]:
+        """
+
+        :param blob:
+        :return:
+        """
+
+        data = blob.copy()
+
+        # Here
+        #  * Exclude records that do not have both coordinate values
+        #  * Extract the records in focus
+        conditionals = data['longitude'].isna() | data['latitude'].isna()
+        excerpt: pd.DataFrame = data.copy().loc[~conditionals, :]
+        excerpt = excerpt.copy().loc[excerpt['pollutant_id'].isin(self.__hazards), :]
+
+        # Structuring
+        structures: list[dict] = excerpt.to_dict(orient='records')
+
+        return [src.elements.sequence.Sequence(**structure)
+                for structure in structures]
+
+    def exc(self, restart: bool) -> list[sq.Sequence]:
+        """
+
+        :param restart:
+        :return:
+        """
+
+        # Retrieve (a) raw references data from Scottish Air & European Environment Information and
+        # Observation Network depositories, or (b) structured references saved in Amazon S3?
+        if restart:
+            registry, stations, substances = src.references.regenerate.Regenerate(
+                service=self.__service, parameters=self.__parameters).exc()
+        else:
+            registry, stations, substances = src.references.read.Read(
+                service=self.__service, parameters=self.__parameters).exc()
+
+        # Merge and structure the references
+        data = self.__integrate(registry=registry, stations=stations, substances=substances)
+        sequences = self.__sequences(blob=data)
+
+        return sequences
