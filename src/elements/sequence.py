bc50a2feae916e3819f71bc6642b79ff4f645a97
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-29 12:17:59 +0000

    Update src/elements/sequence.py

--- src/elements/sequence.py
+++ src/elements/sequence.py
@@ -7,6 +7,17 @@ import typing
 class Sequence(typing.NamedTuple):
     """
     The data type class -> Sequence
+
+    :var:
+      sequence_id: The identification code of the sequence the telemetric device records.
+      unit_of_measure: The unit of measure of the recordings.
+      station_id: The identification code of the station that hosts the telemetric device.
+      pollutant_id: The identification code of the pollutant the telemetric device measures.
+      station_label: Address, etc., details of the station.
+      longitude: The x geographic coordinate.
+      latitude: The y geographic coordinate.
+      substance: The name, and more, of the pollutant.
+      notation: The chemical formula of the pollutant.
     """
 
     sequence_id: int
