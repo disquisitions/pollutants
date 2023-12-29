8d0a73e047b3bc95bfacb15fe7b6bd309e2df110
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-29 14:24:03 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -2,10 +2,9 @@
 import logging
 import os
 
-import pandas as pd
-
 import src.data.points
-import src.elements.sequence
+import src.data.depositories
+import src.elements.sequence as sq
 import src.functions.directories
 import src.references.registry
 
@@ -15,40 +14,31 @@ class Interface:
     Class Interface
     """
 
-    def __init__(self, hazards: list[int]):
+    def __init__(self, sequences: list[sq.Sequence]):
         """
 
-        :param hazards:
+        :param sequences
         """
 
-        self.__hazards = hazards
-
-        self.__directories = src.functions.directories.Directories()
+        self.__sequences = sequences
         self.__storage = os.path.join(os.getcwd(), 'warehouse', 'pollutants', 'points')
 
-    def __sequences(self) -> list[src.elements.sequence.Sequence]:
-        """
-
-        :return:
-        """
-
-        sequences = src.references.registry.Registry().exc()
-        instances: pd.DataFrame = sequences.loc[sequences['pollutant_id'].isin(self.__hazards), :]
-        structures: list[dict] = instances.to_dict(orient='records')
-
-        return [src.elements.sequence.Sequence(**structure) for structure in structures]
-
-    def exc(self, datestr_: list[str]):
+    def exc(self, datestr_: list[str], restart: bool):
         """
 
         :param datestr_:
+        :param restart
         :return:
         """
 
-        # The sequences associated with the pollutant in question
+        if restart:
+            src.functions.directories.Directories().cleanup(self.__storage)
+
+        src.data.depositories.Depositories(
+            sequences=self.__sequences, storage=self.__storage).exc()
+
         # Retrieving data per date, but for several stations in parallel
-        sequences = self.__sequences()
-        points = src.data.points.Points(sequences=sequences, storage=self.__storage)
+        points = src.data.points.Points(sequences=self.__sequences, storage=self.__storage)
         for datestr in datestr_:
             messages = points.exc(datestr=datestr)
             logging.log(level=logging.INFO, msg=messages)
