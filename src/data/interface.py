82aed282d81acbeb9a1b9c6ce5815bad0779d1e0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-12-30 00:44:45 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -4,6 +4,8 @@ import os
 
 import src.data.points
 import src.data.depositories
+import src.elements.parameters as pr
+import src.elements.service as sr
 import src.elements.sequence as sq
 import src.functions.directories
 import src.references.registry
@@ -14,14 +16,18 @@ class Interface:
     Class Interface
     """
 
-    def __init__(self, sequences: list[sq.Sequence], warehouse: str):
+    def __init__(self, service: sr.Service, parameters: pr.Parameters, sequences: list[sq.Sequence], warehouse: str):
         """
 
         :param sequences
         :param warehouse
         """
 
+        self.__service = service
+        self.__parameters = parameters
         self.__sequences = sequences
+
+        # Storage
         self.__storage = os.path.join(warehouse, 'pollutants', 'points')
         src.data.depositories.Depositories(
             sequences=self.__sequences, storage=self.__storage).exc()
@@ -33,8 +39,9 @@ class Interface:
         :return:
         """
 
-        # Retrieving data per date, but for several stations in parallel
-        points = src.data.points.Points(sequences=self.__sequences, storage=self.__storage)
+        # Retrieving data per date, but for several stations & pollutants in parallel
+        points = src.data.points.Points(
+            service=self.__service, parameters=self.__parameters, sequences=self.__sequences, storage=self.__storage)
         for datestr in datestr_:
             messages = points.exc(datestr=datestr)
             logging.log(level=logging.INFO, msg=messages)
