5716b1f8275fd5fb260059fcd7aa525f672e50bc
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-05 16:49:06 +0000

    Update src/s3/sync.py

--- src/s3/sync.py
+++ src/s3/sync.py
@@ -1,14 +1,25 @@
+import json
 import logging
-import subprocess
 import platform
+import subprocess
 
 
 class Sync:
+    """
+    Class Sync
+
+    Description
+    -----------
+
+    Transfers files to Amazon S3
+
+    DataSync incurs cost
+        https://docs.aws.amazon.com/datasync/latest/userguide/create-s3-location.html#create-s3-location-s3-requests
+    """
 
     def __init__(self, restart: bool, profile: str):
         """
-        DataSync incurs cost
-        https://docs.aws.amazon.com/datasync/latest/userguide/create-s3-location.html#create-s3-location-s3-requests
+
         """
 
         self.__restart: bool = restart
@@ -24,6 +35,9 @@ class Sync:
     def exc(self, source: str, destination: str, metadata: dict):
         """
 
+        :param source: The local directory
+        :param destination: The Amazon S3 destination
+        :param metadata: The metadata dictionary
         :return:
         """
 
@@ -32,7 +46,16 @@ class Sync:
         else:
             action = 'cp'
 
-        message = subprocess.run(
-            f'aws s3 {action} {source} s3://{destination} --metadata {metadata} --profile {self.__profile}',
-            shell=self.__shell, capture_output=True)
+        cheat = '"epoch_ms"="The milliseconds unix epoch time when the measure was recorded",' + \
+                '"measure"="The unit of measure of the pollutant under measure",' + \
+                '"timestamp"="The timestamp of the measure",' + \
+                '"date"="The date the measure was recorded",' + \
+                '"sequence_id"="The identification code of the sequence this record is part of."'
+
+        self.__logger.info(f"""aws s3 {action} {source} {destination} """ +
+                           f"""--metadata '{json.dumps(metadata)}' --profile {self.__profile}""")
+
+        message = subprocess.run(f"""aws s3 {action} {source} {destination} """ +
+                                 f"""--metadata {cheat} --profile {self.__profile}""",
+                                 shell=self.__shell, capture_output=True)
         self.__logger.info(message)
