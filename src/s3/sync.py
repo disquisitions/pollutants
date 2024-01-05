5bda0005ad19e24f4e4b85cc9484f23089a96270
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-05 12:53:45 +0000

    Update src/s3/sync.py

--- src/s3/sync.py
+++ src/s3/sync.py
@@ -1,4 +1,4 @@
-
+import logging
 import subprocess
 import platform
 
@@ -12,9 +12,14 @@ class Sync:
         """
 
         self.__restart: bool = restart
-        self.__platform: str = platform.system().lower()
-        self.__shell = False if self.__platform == 'windows' else True
+        self.__shell = False if platform.system().lower() == 'windows' else True
         self.__profile = profile
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
 
     def exc(self, source: str, destination: str, metadata: dict):
         """
@@ -30,3 +35,4 @@ class Sync:
         message = subprocess.run(
             f'aws s3 {action} {source} s3://{destination} --metadata {metadata} --profile {self.__profile}',
             shell=self.__shell, capture_output=True)
+        self.__logger.info(message)
