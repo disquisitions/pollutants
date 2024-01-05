fe9b7f1eedd3eda8f08a25eeac980242649632f0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-05 12:37:43 +0000

    Update src/s3/sync.py

--- src/s3/sync.py
+++ src/s3/sync.py
@@ -4,7 +4,7 @@ import platform
 
 class Sync:
 
-    def __init__(self, restart: bool):
+    def __init__(self, restart: bool, profile: str):
         """
         DataSync incurs cost
         https://docs.aws.amazon.com/datasync/latest/userguide/create-s3-location.html#create-s3-location-s3-requests
@@ -13,9 +13,9 @@ class Sync:
         self.__restart: bool = restart
         self.__platform: str = platform.system().lower()
         self.__shell = False if self.__platform == 'windows' else True
+        self.__profile = profile
 
-
-    def exc(self):
+    def exc(self, source: str, destination: str):
         """
 
         :return:
@@ -24,7 +24,8 @@ class Sync:
         # {} source
         # s3://{} destination
         # profile
-        subprocess.run('aws s3 syn {} s3://{} --profile {}')
+        message = subprocess.run(f'aws s3 sync {source} s3://{destination} --profile {self.__profile}',
+                                 shell=self.__shell, capture_output=True)
 
 
 
