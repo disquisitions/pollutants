6a6662e17df5355035b7218d0975f09f15304975
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-05 12:44:13 +0000

    Update config.py

--- config.py
+++ config.py
@@ -17,5 +17,11 @@ class Config:
         """
 
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+
+        # After the development phase, '732 days', i.e., 2 years.
         self.span = '5 days'
-        self.hazards = [1, 5]
+
+        # 1 Sulphur Dioxide
+        # 5 Particulate matter < 10 µm (aerosol)
+        # 8 Nitrogen Dioxide (air)
+        self.hazards = [1, 8]
