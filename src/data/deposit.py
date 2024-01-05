136095024bda38823825a9b6976021f336b38986
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-05 17:22:41 +0000

    Update src/data/deposit.py

--- src/data/deposit.py
+++ src/data/deposit.py
@@ -1,37 +1,28 @@
+"""
+Module deposit.py
+"""
 import os
 
 import pandas as pd
 
-import src.elements.parameters as pr
-import src.elements.service as sr
 import src.elements.sequence as sq
 import src.functions.streams
 import src.s3.upload
 
 
 class Deposit:
+    """
+    Class Deposit
+    """
 
-    def __init__(self, service: sr.Service, parameters: pr.Parameters, storage: str):
+    def __init__(self, storage: str):
         """
 
-        :param service:
-        :param parameters:
-        :param storage:
+        :param storage: The local data depository
         """
 
-        self.__service = service
-        self.__parameters = parameters
         self.__storage = storage
-
-        self.__upload = src.s3.upload.Upload(service=service, parameters=parameters)
-
         self.__streams = src.functions.streams.Streams()
-
-        self.__metadata = {'epoch_ms': 'The unix epoch time, in milliseconds, when the measure was recorded',
-                           'measure': 'The unit of measure of the pollutant under measure',
-                           'timestamp': 'The timestamp of the measure',
-                           'date': 'The date the measure was recorded',
-                           'sequence_id': 'The identification code of the sequence this record is part of.'}
 
     def __local(self, blob: pd.DataFrame, datestr: str, sequence: sq.Sequence) -> str:
         """
@@ -48,26 +39,12 @@ class Deposit:
 
         return self.__streams.write(blob=blob, path=os.path.join(basename, f'{datestr}.csv'))
 
-    def __s3(self,  blob: pd.DataFrame, datestr: str, sequence: sq.Sequence) -> bool:
-        """
-
-        :param blob:
-        :param datestr:
-        :param sequence:
-        :return:
-            boolean -> Was the item uploaded?
-        """
-
-        key_name = f'{self.__parameters.points_}pollutant_{sequence.pollutant_id}/station_{sequence.station_id}/{datestr}.csv'
-
-        return self.__upload.bytes(data=blob, metadata=self.__metadata, key_name=key_name)
-
     def exc(self, blob: pd.DataFrame, datestr: str, sequence: sq.Sequence) -> str:
         """
 
-        :param blob:
-        :param datestr:
-        :param sequence:
+        :param blob: A telemetric device's sequence data, in the form of a data frame.
+        :param datestr: The date in focus; format YYYY-mm-dd
+        :param sequence: The sequence's details, in collection form.
         :return:
         """
 
