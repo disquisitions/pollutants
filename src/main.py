11277bb2b72cf637bdb35965cc92192d006ad987
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-11 17:55:54 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -25,11 +25,11 @@ def main():
     logger.info('Dates\n%s', datestr_)
 
     # Sequences
-    sequences = src.references.interface.Interface(service=service, parameters=parameters,
-                                                   hazards=configurations.hazards).exc(restart=restart)
+    sequences = src.references.interface.Interface(
+        service=service, parameters=parameters, hazards=configurations.hazards).exc(restart=restart)
 
-    src.data.interface.Interface(parameters=parameters, sequences=sequences, profile=profile,
-                                 warehouse=configurations.warehouse, restart=restart).exc(datestr_=datestr_)
+    src.data.interface.Interface(
+        parameters=parameters, sequences=sequences, profile=profile, restart=restart).exc(datestr_=datestr_)
 
     # Deleting __pycache__
     src.functions.cache.Cache().delete()
