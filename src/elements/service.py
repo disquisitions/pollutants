42fcfb7eb0ad67f19f252914d02631816f9bb8cd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-13 15:57:06 +0000

    Update src/elements/service.py

--- src/elements/service.py
+++ src/elements/service.py
@@ -12,6 +12,13 @@ import src.elements.parameters
 class Service(typing.NamedTuple):
     """
     The data type class -> Service
+
+    Attributes
+    ----------
+    s3_resource: boto3.session.Session.resource
+        The boto3.resource instance, with service & region name settings.
+    s3_client: boto3.session.Session.client
+        The boto3.client instance, with service & region name settings.
     """
 
     s3_resource: boto3.session.Session.resource
