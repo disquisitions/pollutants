f42a13f524a80ec1aba8b72cfa45b3019bad7ba7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-30 23:38:29 +0000

    Update src/functions/service.py

--- src/functions/service.py
+++ src/functions/service.py
@@ -1,13 +1,10 @@
 """
 Module service.py
 """
-import os
 
 import boto3
-import botocore.client
 
 import src.elements.s3_parameters as s3p
-import src.elements.profile as po
 import src.elements.service as sr
 import src.functions.serial
 import src.s3.parameters
@@ -27,52 +24,36 @@ class Service:
             html#boto3.session.Session.resource
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters, profile: po.Profile):
+    def __init__(self):
         """
 
-        :param s3_parameters: The S3 parameters settings for this project
-        :param profile: The developer's Amazon Web Services (AWS) profile detail, which allows
-                        for programmatic interaction with AWS.
-        """
+        Describe
+        --------
 
-        # Profile
-        self.__profile = profile
+        The constructor.
+        """
 
         # Profile/Auto-login
+        # This session will retrieve the developer's <default> Amazon Web Services (AWS) profile
+        # details, which allows for programmatic interaction with AWS.
         connector = boto3.session.Session()
 
         # The S3 resource, client, etc.
         self.__s3_resource: boto3.session.Session.resource = connector.resource(
-            service_name='s3', region_name=s3_parameters.region_name)
+            service_name='s3')
         self.__s3_client: boto3.session.Session.client = connector.client(
-            service_name='s3', region_name=s3_parameters.region_name)
+            service_name='s3')
         self.__secrets_manager = connector.client(
-            service_name='secretsmanager', region_name=s3_parameters.region_name)
-        self.__glue_client: botocore.client.BaseClient = connector.client(
-            service_name='glue', region_name=s3_parameters.region_name)
-
-    def __glue_arn(self) -> str:
-        """
-
-        :return:
-        """
-
-        # Amazon Resource Name (ARN)
-        serial = src.functions.serial.Serial()
-        glue_arn: str = serial.get_dictionary(uri=os.path.join(os.getcwd(), 'resources', 'arn.yaml'))['arn']['glue']
-
-        return glue_arn.format(account_id=self.__profile.account_id)
+            service_name='secretsmanager')
 
     def exc(self) -> src.elements.service.Service:
         """
 
         :return:
-            A collection of Amazon S3 (Simple Storage Service) services
+            A collection of Amazon services
         """
 
         # Hence, the collection
         return src.elements.service.Service(s3_resource=self.__s3_resource,
                                             s3_client=self.__s3_client,
-                                            secrets_manager=self.__secrets_manager,
-                                            glue_client=self.__glue_client,
-                                            glue_arn=self.__glue_arn())
+                                            secrets_manager=self.__secrets_manager)
