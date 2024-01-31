8a46e2d34bee5eea1e8ae2221f0c05bf4187f1b9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-31 19:30:32 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -70,8 +70,7 @@ class Interface:
 
         # Retrieving data per date, but for several stations & pollutants in parallel
         points = src.data.points.Points(sequences=self.__sequences, storage=self.__storage)
-        for datestr in datestr_:
-            messages = points.exc(datestr=datestr)
-            logging.log(level=logging.INFO, msg=messages)
+        messages = points.exc()
+        logging.log(level=logging.INFO, msg=messages)
 
         self.__s3()
