3878f9b9f4e77180591bd4fac63724d30e0a89eb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-31 23:13:57 +0000

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
         self.points_storage = os.path.join(self.warehouse, 'environment', 'pollutants', 'points')
 
         # After the development phase, '732 days', i.e., 2 years.
-        self.span = '2 days'
+        self.span = '28 days'
 
         # 1 Sulphur Dioxide [SO2]
         # 5 Particulate matter < 10 µm (aerosol)
