88c88e0262461b2a1612d92cf92f8f25fa771c9e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-01-31 00:06:33 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -21,48 +21,56 @@ def main():
     sequences = src.references.interface.Interface(
         service=service, s3_parameters=s3_parameters).exc(restart=restart)
     src.data.interface.Interface(
-        s3_parameters=s3_parameters, sequences=sequences, profile=profile, restart=restart).exc(datestr_=datestr_)
+        s3_parameters=s3_parameters, sequences=sequences, restart=restart).exc(datestr_=datestr_)
 
     # Deleting __pycache__
     src.functions.cache.Cache().delete()
     
 
 if __name__ == '__main__':
-
-    # Paths
+    '''
+    Setting-up
+    '''
     root = os.getcwd()
     sys.path.append(root)
     sys.path.append(os.path.join(root, 'src'))
 
-    # Logging
     logging.basicConfig(level=logging.INFO,
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
     
-    # Modules
+    '''
+    Modules
+    '''
     import config
     import src.algorithms.dates
     import src.data.interface
+
     import src.elements.s3_parameters as s3p
-    import src.elements.profile as po
     import src.elements.service as sr
+
     import src.functions.cache
-    import src.functions.profile
     import src.functions.service
+
     import src.references.interface
     import src.s3.parameters
     import src.setup
 
-    # Upcoming arguments:
-    # If restart then all the pollutants data retrieved thus far will be deleted from the cloud depository.
+    '''
+    Upcoming arguments:
+    If restart then all the pollutants data retrieved thus far will be deleted from the cloud depository.
+    '''
     restart = True
 
-    # S3 Parameters, Profile, Service Instance
+    '''
+    S3 Parameters, Service Instance
+    '''
     s3_parameters: s3p.S3Parameters = src.s3.parameters.Parameters().exc()
-    profile: po.Profile = src.functions.profile.Profile().exc()
-    service: sr.Service = src.functions.service.Service(s3_parameters=s3_parameters, profile=profile).exc()
+    service: sr.Service = src.functions.service.Service().exc()
 
-    # Setting-up
+    '''
+    Setting-up
+    '''
     configurations = config.Config()
     restart = src.setup.Setup(service=service, s3_parameters=s3_parameters, warehouse=configurations.warehouse).exc(
         restart=restart)
