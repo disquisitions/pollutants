5fa02aba6723e024a8862b8c3c7549860884c09d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-11 01:56:15 +0000

    Update src/data/api.py: A new time span structure.

--- src/data/api.py
+++ src/data/api.py
@@ -1,4 +1,7 @@
 """Module api.py"""
+import logging
+
+import pandas as pd
 
 
 class API:
@@ -11,8 +14,12 @@ class API:
         Constructor
         """
 
-    @staticmethod
-    def __pattern(sequence_id: int, datestr: str) -> str:
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def __pattern(self, sequence_id: int, datestr: str) -> str:
         """
 
         :param sequence_id: The identification code of an air pollutant sequence
@@ -20,8 +27,12 @@ class API:
         :return
         """
 
+        ending = str(pd.date_range(start=datestr, periods=1, freq='ME').to_list()[0].date())
+
         string = f"""https://www.scottishairquality.scot/sos-scotland/api/v1/timeseries/{sequence_id}/getData?""" + \
-                 f"""expanded=true&phenomenon=1&format=highcharts&timespan={datestr}T00:00:00Z/{datestr}T23:59:59Z"""
+                 f"""expanded=true&phenomenon=1&format=highcharts&timespan={datestr}T00:00:00Z/{ending}T23:59:59Z"""
+
+        self.__logger.info(string)
 
         return string
 
