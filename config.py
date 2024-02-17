9b57b5ff4dba6961a4db9c8bac346555297a0ad5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-17 19:30:51 +0000

    Update config.py

--- config.py
+++ config.py
@@ -22,16 +22,19 @@ class Config:
         # After the development phase, 732 days, i.e., 2 years.
         self.span: int = 366
 
+        # Hazardous items in focus
         # 1 Sulphur Dioxide [SO2]
         # 5 Particulate matter < 10 µm (aerosol)
         # 8 Nitrogen Dioxide (air) [NO2]
         # 38 Nitrogen Monoxide [NO]
-        self.hazards = [1, 38]
+        # self.hazards = [1, 38]
 
         # Devices in focus, via their series codes
         # station: 907, sequence 161, SO2 | station: 900, sequence 154, NO | station: 1013, sequence 530, NO | station: 136,
         # sequence: 228, NO2 | station: 148, sequence: 212, NO2
-        self.sequence_id_filter = [228, 212]
+        #
+        # sequences: 142, 169, 155, 531, 165, 173, 177, 146, 150 (NO2, EDB)
+        self.sequence_id_filter = [142, 169, 155, 531, 165, 173, 177, 146, 150]
 
         # The points metadata
         self.metadata = {'epoch_ms': 'The milliseconds unix epoch time  when the measure was recorded',
