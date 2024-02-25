d93279017f2b033cc895fc2ffc3472e0f700cfdb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-25 19:53:52 +0000

    Update src/elements/s3_parameters.py

--- src/elements/s3_parameters.py
+++ src/elements/s3_parameters.py
@@ -6,7 +6,7 @@ import typing
 
 class S3Parameters(typing.NamedTuple):
     """
-    The data type class -> S3Parameters
+    The data type class ⇾ S3Parameters
 
     Attributes
     ----------
@@ -19,23 +19,19 @@ class S3Parameters(typing.NamedTuple):
     access_control_list : str
       Access control list selection.
 
-    bucket_name : str
+    bucket_name_int : str
       The Amazon S3 (Simple Storage Service) bucket that hosts this project's data.
 
-    points_ : str
+    path_int_points : str
       The bucket path of the telemetric data.
 
-    references_ : str
+    path_int_references : str
       The bucket path of the telemetric data references.
-
-    n_references : int
-      The exact number of reference documents.
     """
 
     region_name: str
     location_constraint: str
     access_control_list: str
-    bucket_name: str
-    points_: str
-    references_: str
-    n_references: int
+    bucket_name_int: str
+    path_int_points: str
+    path_int_references: str
