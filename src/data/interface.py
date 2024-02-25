e85cd68311e417c95cf9558f1d9ac089f6957eb6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-25 17:06:19 +0000

    Update src/data/interface.py

--- src/data/interface.py
+++ src/data/interface.py
@@ -1,15 +1,10 @@
 """Module interface.py"""
 import logging
 
-import config
 import src.data.depositories
 import src.data.points
 import src.elements.s3_parameters as s3p
 import src.elements.sequence as sq
-import src.functions.directories
-import src.references.registry
-import src.s3.ingress
-import src.s3.sync
 
 
 class Interface:
@@ -17,58 +12,26 @@ class Interface:
     Class Interface
     """
 
-    def __init__(self, s3_parameters: s3p.S3Parameters, sequences: list[sq.Sequence], restart: bool):
+    def __init__(self, s3_parameters: s3p.S3Parameters, sequences: list[sq.Sequence]):
         """
 
         :param s3_parameters: The S3 parameters settings for this project
         :param sequences: Each list item is the detail of a sequence, in collection form.
-        :param restart: Restart?  If yes, it means all previous cloud data
-                        will be, has been, deleted during this run.
         """
 
         self.__s3_parameters = s3_parameters
         self.__sequences = sequences
-        self.__sync = src.s3.sync.Sync(restart=restart)
 
-        # Storage
-        configurations = config.Config()
-        self.__storage = configurations.points_storage
+    def exc(self, storage: str):
+        """
+
+        :return:
+        """
+
         src.data.depositories.Depositories(
-            sequences=self.__sequences, storage=self.__storage).exc()
-
-    @staticmethod
-    def __metadata() -> str:
-        """
-
-        :return: The metadata of the data points being transferred to Amazon S3
-        """
-
-        metadata = '"epoch_ms"="The milliseconds unix epoch time  when the measure was recorded",' + \
-                   '"measure"="The unit of measure of the pollutant under measure",' + \
-                   '"timestamp"="The timestamp of the measure",' + \
-                   '"date"="The date the measure was recorded",' + \
-                   '"sequence_id"="The identification code of the sequence this record is part of."'
-
-        return metadata
-
-    def __s3(self):
-        """
-        Bulk transfer of files to Amazon S3
-
-        :return:
-        """
-
-        self.__sync.exc(source=self.__storage,
-                        destination=f's3://{self.__s3_parameters.bucket_name}/{self.__s3_parameters.points_}',
-                        metadata=self.__metadata())
-
-    def exc(self):
-        """
-
-        :return:
-        """
+            sequences=self.__sequences, storage=storage).exc()
 
         # Retrieving data per date, but for several stations & pollutants in parallel
-        points = src.data.points.Points(sequences=self.__sequences, storage=self.__storage)
+        points = src.data.points.Points(sequences=self.__sequences, storage=storage)
         messages = points.exc()
         logging.log(level=logging.INFO, msg=messages)
