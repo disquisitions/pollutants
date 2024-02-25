f84f18a609a70923b76b504de8de6e78b19f2437
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-25 17:53:14 +0000

    Update config.py

--- config.py
+++ config.py
@@ -28,10 +28,3 @@ class Config:
         # area: Edinburgh
         # sequence (station): 155 (901), 531 (1014), 177 (460), 150 (791) 165(327), 142 (196)
         self.sequence_id_filter = [155, 531, 177, 150]
-
-        # The points metadata
-        self.metadata = {'epoch_ms': 'The milliseconds unix epoch time  when the measure was recorded',
-                         'measure': 'The unit of measure of the pollutant under measure',
-                         'timestamp': 'The timestamp of the measure',
-                         'date': 'The date the measure was recorded',
-                         'sequence_id': 'The identification code of the sequence this record is part of.'}
