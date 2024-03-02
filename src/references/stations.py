82c01cb2733fd7e952b3805dfafcbe49ee768b09
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-02 00:13:24 +0000

    Update src/references/stations.py

--- src/references/stations.py
+++ src/references/stations.py
@@ -1,5 +1,4 @@
 """Module stations.py"""
-import logging
 
 import pandas as pd
 
@@ -27,12 +26,6 @@ class Stations:
         # Metadata instance
         self.__metadata = src.references.metadata.Metadata().stations()
 
-        # Logging
-        logging.basicConfig(level=logging.INFO,
-                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
-                            datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
-
     @staticmethod
     def __structure(blob: dict) -> pd.DataFrame:
         """
@@ -44,7 +37,7 @@ class Stations:
         try:
             normalised = pd.json_normalize(data=blob, max_level=2)
         except ImportError as err:
-            raise Exception(err) from err
+            raise err from err
 
         coordinates = pd.DataFrame(data=normalised['geometry.coordinates'].to_list(),
                                    columns=['longitude', 'latitude', 'height'])
