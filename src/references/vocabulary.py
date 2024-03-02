483f9196636b766e797ed6689c7ab93b1ac4fb89
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-02 00:13:48 +0000

    Update src/references/vocabulary.py

--- src/references/vocabulary.py
+++ src/references/vocabulary.py
@@ -34,7 +34,6 @@ class Vocabulary:
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
-        self.__logger = logging.getLogger(__name__)
 
     @staticmethod
     def __feature_engineering(blob: pd.DataFrame) -> pd.DataFrame:
