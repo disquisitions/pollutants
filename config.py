6cbd8ca9a33adadbc4c65297254970ac36db44eb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-02 19:52:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Config:
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
 
         # After the development phase the dates will be different
-        self.starting = datetime.datetime.strptime('2022-01-01', '%Y-%m-%d')
-        self.ending = datetime.datetime.today()
+        self.starting = datetime.datetime.strptime('2023-01-01', '%Y-%m-%d')
+        self.ending = datetime.datetime.today() - datetime.timedelta(days=3)
 
         # Devices in focus, via their sequence identifiers
         # pollutant: Nitrogen Dioxide
