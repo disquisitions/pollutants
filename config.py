91716be7bf957c5ae8b317dd99b5efa0beba007f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-04-06 00:11:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
 
         # After the development phase the start date will be a few years earlier.
-        self.starting = datetime.datetime.strptime('2018-01-01', '%Y-%m-%d')
+        self.starting = datetime.datetime.strptime('2011-01-01', '%Y-%m-%d')
         self.ending = datetime.datetime.today() - datetime.timedelta(days=3)
 
         # Devices in focus, via their sequence identifiers
